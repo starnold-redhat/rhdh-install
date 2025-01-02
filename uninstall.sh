c9402dc5cf52c936523c7616f513dd9a0d0b9b73
user1 GitLab <user1@demo.redhat.com>
2025-01-02 12:33:06 +0000

    latest

--- uninstall.sh
+++ uninstall.sh
@@ -6,6 +6,7 @@ oc delete keycloakclient openshift -n rhsso
 oc delete keycloakrealm backstage -n rhsso 
 oc delete keycloakrealm openshift -n rhsso 
 oc delete keycloak rhsso-instance -n rhsso 
+oc delete automationcontroller controller -n aap
 oc delete project rhsso
 oc delete project gitlab
 oc delete project backstage

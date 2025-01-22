6871786afa4af8be9cf3dcc906c0530870c23d5f
user1 GitLab <user1@demo.redhat.com>
2025-01-22 13:25:40 +0000

    fixed devspaces url

--- install.sh
+++
@@ -1,7 +0,0 @@
-export OCP_BASE=apps.cluster-jd47r.jd47r.sandbox808.opentlc.com
-oc new-project rhsso
-oc apply -f sso_operator.yaml
-oc wait crd --timeout=-1s keycloaks.keycloak.org   --for=condition=Established
-oc apply -f sso_keycloak.yaml
-
-
